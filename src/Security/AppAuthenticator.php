d9de6e0d01950770fe05ee2b248afb4b8861a32f
Maxime-eni <maxime.thuault2021@campus-eni.fr>
2021-05-31 21:32:23 +0200

    21h30, test de connection -> Problème à regler

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -95,7 +95,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
             return new RedirectResponse($targetPath);
         }
 
-       return new RedirectResponse($this->urlGenerator->generate('accueil_sortie'));
+        return new RedirectResponse($this->urlGenerator->generate('main_index'));
     }
 
     protected function getLoginUrl()
