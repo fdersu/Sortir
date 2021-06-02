9596fa3024c3012ed5662a8d235c54b0277542ae
Julie <ju_apps@gmx.com>
2021-06-02 14:43:25 +0200

    tjs non fonctionnel

--- src/Form/SortieFormType.php
+++ src/Form/SortieFormType.php
@@ -15,6 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SortieFormType extends AbstractType
@@ -36,31 +37,6 @@ class SortieFormType extends AbstractType
                 'time_widget' => 'single_text'
             ])
             ->add('duree', NumberType::class)
-            ->add('ville', EntityType::class, [
-                'class' => Ville::class,
-                'mapped' => false,
-                'choice_label' => 'nom'
-            ])
-
-            ->addEventListener(FormEvents::POST_SET_DATA, function ($event) {
-                $form = $event->getForm(); // The FormBuilder
-                $sortie = $event->getData(); // The Form Object
-                $ville = $form->get('ville')->getData();
-
-
-               dump($ville);
-               dump($sortie);
-
-               $lieux = $this->entityManager->getRepository(Lieu::class)->findBy(['ville' => $ville]);
-
-               $form->add('lieu', ChoiceType::class, [
-                   'choices' => $lieux,
-                   'choice_label' => 'nom'
-                   ]);
-
-               $event->setData($sortie);
-            })
-
             ->add('description', TextareaType::class)
             ->add('nbInscriptionsMax')
             ->add('dateCloture', DateTimeType::class, [
@@ -68,13 +44,52 @@ class SortieFormType extends AbstractType
                 'date_widget' => 'single_text',
                 'time_widget' => 'single_text'
             ])
-        ;
+            ->add('ville', EntityType::class, [
+                'class' => Ville::class,
+                'mapped' => false,
+                'placeholder' => 'Choisissez une ville',
+                'choice_label' => 'nom'
+            ]);
+
+            $formModifier = function (FormInterface $form, Ville $ville = null) {
+                $lieux = null === $ville ? [] : $this->entityManager->getRepository(Lieu::class)->findBy(['ville' => $ville]);
+
+                $form->add('lieu', EntityType::class, [
+                    'class' => Lieu::class,
+                    'placeholder' => 'Choisissez un lieu',
+                    'choices' => $lieux,
+                ]);
+            };
+
+            $builder->addEventListener(
+                FormEvents::PRE_SET_DATA,
+                function ($event) use ($formModifier){
+                    $form = $event->getForm(); // The FormBuilder
+                    $sortie = $event->getData(); // The Form Object (unused here)
+                    $ville = $form->get('ville')->getData();
+
+                    $formModifier($form, $ville);
+
+            });
+
+            $builder->get('ville')->addEventListener(
+                FormEvents::POST_SUBMIT,
+                function (FormEvent $event) use($formModifier){
+
+                    $ville = $event->getForm()->get('ville')->getData();
+                    
+                    $formModifier($event->getForm()->getParent(), $ville);
+                }
+            );
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Sortie::class,
+            'compound' => true,
+            'inherit_data' => true,
         ]);
     }
 }
