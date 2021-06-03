48ee931bfe61d9947bfc724b0475eb0e0a3c0844
Maxime-eni <maxime.thuault2021@campus-eni.fr>
2021-06-03 15:46:20 +0200

    15h30: Recuperation de tout les codes

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -9,7 +9,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
