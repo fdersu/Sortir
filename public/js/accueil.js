51913cf157a3ad878210365af64465fa6eba050b
fdersu <fredericdersu@gmail.com>
2021-06-10 09:51:12 +0200

    commit css buttons + motif ajax appear first click, disappear next click

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -4,30 +4,30 @@ function displayReason() {
     let etat = document.getElementsByClassName('etat');
     for(let item of etat){
         item.addEventListener('click', function () {
-            let sortieId = item.parentElement.lastElementChild.value;
-            console.log(sortieId);
-            let parent = getParentDiv(item, 7);
-            console.log(parent);
-            let divRow = document.createElement('div');
-            divRow.className = 'row';
-            divRow.id = 'ajax';
-            let divCol = document.createElement('div');
-            divCol.className = 'col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 col-xxl-8 ' +
-                                'offset-0 offset-sm-0 offset-md-4 offset-lg-2 offset-xl-2 offset-xxl-2';
-            divCol.id = 'ajaxDisplay';
-            divCol.style.backgroundColor = 'white';
-            let motif = document.createElement('strong');
-            getMotif(motif, sortieId);
-            divCol.appendChild(motif);
-            divRow.appendChild(divCol);
-            parent.appendChild(divRow);
-        })
-        item.addEventListener('mouseover', function () {
             if(document.getElementById('ajax') != null) {
                 let toDelete = document.getElementById('ajax');
                 toDelete.remove();
+            }else {
+                let sortieId = item.parentElement.lastElementChild.value;
+                console.log(sortieId);
+                let parent = getParentDiv(item, 7);
+                console.log(parent);
+                let divRow = document.createElement('div');
+                divRow.className = 'row';
+                divRow.id = 'ajax';
+                let divCol = document.createElement('div');
+                divCol.className = 'col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 col-xxl-8 ' +
+                    'offset-0 offset-sm-0 offset-md-4 offset-lg-2 offset-xl-2 offset-xxl-2';
+                divCol.id = 'ajaxDisplay';
+                divCol.style.backgroundColor = 'white';
+                let motif = document.createElement('strong');
+                getMotif(motif, sortieId);
+                divCol.appendChild(motif);
+                divRow.appendChild(divCol);
+                parent.appendChild(divRow);
             }
         })
+
     }
 }
 
